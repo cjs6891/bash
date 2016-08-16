74deca54201c431dc033a36f33b437c27fb34302
cjs6891 <cjs6891@outlook.com>
2016-08-16 10:46:15 -0400

    Revert "Update PublicIP.sh"
    This reverts commit 51cbbbaa113e1508a4577bb86d8ba14fb56e6cfe.

--- PublicIP.sh
+++ PublicIP.sh
@@ -5,14 +5,3 @@ dig="/usr/bin/dig"
 awk="/usr/bin/awk"
 
 $dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | $awk -F'"' '{ print $2}'
-
-# # # # #
- # # # #
-  # # #
-   # #
-    #
-    #
-   # #
-  # # #
- # # # #
-# # # # #
