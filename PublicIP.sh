51cbbbaa113e1508a4577bb86d8ba14fb56e6cfe
cjs6891 <cjs6891@outlook.com>
2016-08-16 10:41:26 -0400

    Update PublicIP.sh

--- PublicIP.sh
+++ PublicIP.sh
@@ -5,3 +5,14 @@ dig="/usr/bin/dig"
 awk="/usr/bin/awk"
 
 $dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | $awk -F'"' '{ print $2}'
+
+# # # # #
+ # # # #
+  # # #
+   # #
+    #
+    #
+   # #
+  # # #
+ # # # #
+# # # # #
