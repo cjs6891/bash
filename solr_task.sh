aad7478c5782c024d4a415bf68c026f38864f2b9
cjs6891 <cjs6891@outlook.com>
2020-09-16 10:36:34 -0400

    Create solr_task.sh

---
+++ solr_task.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+function solr_task
+{
+    message=""
+    subject="$HOSTNAME - Solr Administration Console Update"
+    from="no-reply@wshein.com"
+    to1="cjs@wshein.com"
+    to2=""
+    option=NULL
+    configuration_files="/opt/administration_console/configuration_files/solr"
+    clear
+    echo -e "\e[93m##############################"
+    echo -e "\e[93m - Solr Administration Console"
+    echo -e "\e[93m##############################"
+    echo -e "\e[93m------------------------------"
+
+    until [ "$option" = "a" ] || \
+        [ "$option" = "b" ] || \
+        [ "$option" = "c" ] || \
+        [ "$option" = "d" ] || \
+        [ "$option" = "e" ] || \
+        [ "$option" = "0" ]
+    do
+        echo "  a) Live Server"
+        echo "  b) Transfer Load: galera210"
+        echo "  c) Transfer Load: galera212"
+        echo "  d) Transfer Load: galera214"
+        echo "  e) Transfer Load: standalone33"
+        echo "  0) Main Menu"
+
+        echo -n "Enter Choice: "
+        read option
+        echo ""
+        case $option in
+            a ) cat /etc/nginx/conf.d/solr.conf\
+             | grep -A1 -E "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"\
+             | grep -E "server" | awk '{print "Live Server: "$2}'
+                sleep 5
+                solr_task
+                ;;
+            b ) sudo cp $configuration_files/solr210.conf /etc/nginx/conf.d/solr.conf
+                sudo nginx -t && sudo nginx -s reload
+                today=$(date '+%F %T')
+                message="SSH Client: $SSH_CLIENT Transferred Solr Load --to--> galera210 on $HOSTNAME at $today"
+                if [[ $(echo $?) = 0 ]]
+                then
+                    echo $message
+                    echo $message | mailx -r $from -s "$subject" $to1
+                    echo $message | mailx -r $from -s "$subject" $to2
+                fi
+                sleep 5
+                solr_task
+                ;;
+            c ) sudo cp $configuration_files/solr212.conf /etc/nginx/conf.d/solr.conf
+                sudo nginx -t && sudo nginx -s reload
+                today=$(date '+%F %T')
+                message="SSH Client: $SSH_CLIENT Transferred Solr Load --to--> galera212 on $HOSTNAME at $today"
+                if [[ $(echo $?) = 0 ]]
+                then
+                    echo $message
+                    echo $message | mailx -r $from -s "$subject" $to1
+                    echo $message | mailx -r $from -s "$subject" $to2
+                fi
+                sleep 5
+                solr_task
+                ;;
+            d ) sudo cp $configuration_files/solr214.conf /etc/nginx/conf.d/solr.conf
+                sudo nginx -t && sudo nginx -s reload
+                today=$(date '+%F %T')
+                message="SSH Client: $SSH_CLIENT Transferred Solr Load --to--> galera214 on $HOSTNAME at $today"
+                if [[ $(echo $?) = 0 ]]
+                then
+                    echo $message
+                    echo $message | mailx -r $from -s "$subject" $to1
+                    echo $message | mailx -r $from -s "$subject" $to2
+                fi
+                sleep 5
+                solr_task
+                ;;
+            e ) sudo cp $configuration_files/standalone33.conf /etc/nginx/conf.d/solr.conf
+                sudo nginx -t && sudo nginx -s reload
+                today=$(date '+%F %T')
+                message="SSH Client: $SSH_CLIENT Transferred Solr Load --to--> standalone33 on $HOSTNAME at $today"
+                if [[ $(echo $?) = 0 ]]
+                then
+                    echo $message
+                    echo $message | mailx -r $from -s "$subject" $to1
+                    echo $message | mailx -r $from -s "$subject" $to2
+                fi
+                sleep 5
+                solr_task
+                ;;
+            0 ) main_menu ;;
+            * ) echo "Incorrect Choice"
+                sleep 1
+                solr_task
+                ;;
+        esac
+    done
+}
